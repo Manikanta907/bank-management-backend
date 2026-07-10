ea28a4ed707421770946318187cf6a3f4158d4a5
manikanta907 <tanukulapeddiraju6@gmail.com>
2026-07-10 14:10:52 +0530

    Fix CORS for Render frontend

--- src/main/java/com/Banking_System/internshipproject/Controller/TransactionController.java
+++ src/main/java/com/Banking_System/internshipproject/Controller/TransactionController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://bank-management-frontendd.onrender.com"
+})
 @RestController
 @RequestMapping("/api/transactions")
 public class TransactionController {
